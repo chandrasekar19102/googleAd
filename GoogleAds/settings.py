1b96e415bff28e219517b14f76da87f1f1e07903
chandrasekar19102 <chandrasekar19102@gmail.com>
2024-04-29 18:44:09 +0530

    allowed host all

--- GoogleAds/settings.py
+++ GoogleAds/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)m=v^a2m@tl)#)_$50d5b%&0qodozxisnl7+lojjmf9@=*r=q&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
